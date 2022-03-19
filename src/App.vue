80b4d2abea480b13bfc0a9b7c5be4ac7906191d9
YoungJinGo <skyyj32@naver.com>
2022-03-19 13:34:56 +0900

    Feat : setup branch pull

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,7 @@
 <template>
-<<<<<<< HEAD
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link> |
-    <router-link to="/detail">Detail</router-link>
-  </nav>
-  <router-view />
-=======
   <div class="container mx-auto px-4 h-screen">
     <router-view name="main"></router-view>
   </div>
->>>>>>> 36ebcbd6ecd85b1dd7b9f66f1b050484d55aad6c
 </template>
 
 <script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,34 +1,7 @@
-<<<<<<< HEAD
-import { createRouter, createWebHashHistory } from 'vue-router';
-import HomeView from '../views/HomeView.vue';
-import DetailInfo from '../views/DetailInfo.vue';
-
-const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView,
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue'),
-  },
-  {
-    path: '/detail',
-    name: 'detail',
-    component: DetailInfo,
-  },
-];
-=======
 import { createRouter, createWebHistory } from 'vue-router';
 import baseRoute from '@/router/base';
 
 const finalRoute = [].concat(baseRoute);
->>>>>>> 36ebcbd6ecd85b1dd7b9f66f1b050484d55aad6c
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
