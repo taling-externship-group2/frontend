8da9c9d58f0eb8f1ff446c372d6fec3a96c87c20
hogyun3709 <hogyun3709@gmail.com>
2022-03-22 21:45:03 +0900

    add(homepage): #6 create components for talent list tab section
    It builds tab components by using dynamic components

---
+++ src/components/Home/TalentBest.vue
@@ -0,0 +1,12 @@
+<template>
+  <div data-test="talent-best">
+    <h1>탈잉 BEST입니다.</h1>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
---
+++ src/components/Home/TalentsList.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="border-2 my-4 px-4 py-2" data-test="tab-area">
+    <ul class="flex justify-around border-2 my-2">
+      <li
+        v-for="tab in talentsList"
+        :key="tab"
+        data-test="talent-list-tab-category"
+      >
+        <button
+          class="my-2 px-4 py-4"
+          @click="initialTab = tab.component"
+          :data-test="`${tab.nameKr}`"
+        >
+          {{ tab.nameKr }}
+        </button>
+      </li>
+    </ul>
+    <component :is="initialTab" class="border-2 px-2 py-2"></component>
+  </div>
+</template>
+
+<script>
+import TalentBest from '@/components/Home/TalentBest.vue';
+import TalentRecommended from '@/components/Home/TalentRecommended.vue';
+
+export default {
+  components: {
+    TalentBest,
+    TalentRecommended,
+  },
+  data() {
+    return {
+      initialTab: 'TalentRecommended',
+      talentsList: [
+        { component: 'TalentRecommended', nameKr: '추천수업' },
+        { component: 'TalentBest', nameKr: '탈잉 BEST' },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+</style>
