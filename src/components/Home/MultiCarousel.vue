807393cb76b61c00b97f4c136f94aafd0eb1d226
hogyun3709 <hogyun3709@gmail.com>
2022-03-28 16:31:33 +0900

    refactor(Hompage): #28 improve reuseability of talent wrapper view by using vue-slot
    minimize reduandancy in talent recommended view with using vue slot to wrap talent-item component by mutli-carousel component

---
+++ src/components/Home/MultiCarousel.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="flex justify-between">
+    <h4 class="p-1 font-bold text-xl">{{ title }}</h4>
+    <div class="">
+      <font-awesome-icon
+        class="text-sm font-thin rounded-full border-2 p-1 m-1"
+        icon="arrow-left"
+        size="lg"
+        @click="prevSlide"
+        data-test="prev-btn"
+      />
+
+      <font-awesome-icon
+        class="text-sm rounded-full border-2 p-1 m-1"
+        icon="arrow-right"
+        size="lg"
+        @click="nextSlide"
+        data-test="next-btn"
+      />
+    </div>
+  </div>
+  <div class="overflow-hidden relative w-full pt-80">
+    <div class="overflow-hidden absolute inset-0 flex flex-nowrap">
+      <div class="w-full flex flex-wrap ease-out duration-300" :id="title">
+        <ul class="flex flex-nowrap">
+          <slot></slot>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      slideIndex: 0,
+      slideLength: this.totalSlide,
+    };
+  },
+  props: {
+    title: { type: String, default: '' },
+    totalSlide: { type: Number, default: -1 },
+  },
+  methods: {
+    slideAction() {
+      const carouselContent = document.getElementById(`${this.title}`);
+      if (carouselContent) {
+        const size = carouselContent.clientWidth;
+        carouselContent.style.transform = `translateX(-${
+          (this.slideIndex * size) / 3
+        }px)`;
+      }
+    },
+    prevSlide() {
+      if (this.slideIndex !== 0) {
+        this.slideIndex -= 1;
+        this.slideAction();
+      }
+    },
+    nextSlide() {
+      if (this.slideIndex !== this.slideLength - 1) {
+        this.slideIndex += 1;
+        this.slideAction();
+      }
+    },
+  },
+};
+</script>
+
+<style>
+</style>
